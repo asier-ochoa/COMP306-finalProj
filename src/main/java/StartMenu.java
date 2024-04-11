3cb11974bfde7dfcd48edee00efa3f3d2206926c
asier-ochoa <78745593+asier-ochoa@users.noreply.github.com>
2024-04-11 17:16:24 -0500

    Image loading menu done, just need some polish

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -1,10 +1,17 @@
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.Spinner;
 import javafx.fxml.FXML;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 
 public class StartMenu {
+    @FXML
+    private TextField imagePath;
     @FXML
     private Spinner<Integer> horizontalTilesInput;
     @FXML
@@ -20,8 +27,29 @@ public class StartMenu {
     }
 
     private void configureSpinners(){
-        final var factory = new SpinnerValueFactory.IntegerSpinnerValueFactory(4, 26);
-        verticalTilesInput.setValueFactory(factory);
-        horizontalTilesInput.setValueFactory(factory);
+        final var factory1 = new SpinnerValueFactory.IntegerSpinnerValueFactory(4, 26);
+        final var factory2 = new SpinnerValueFactory.IntegerSpinnerValueFactory(4, 26);
+        factory1.setValue(8);
+        factory2.setValue(8);
+        verticalTilesInput.setValueFactory(factory1);
+        horizontalTilesInput.setValueFactory(factory2);
+    }
+
+    @FXML
+    private void browseFiles(ActionEvent event){
+        event.consume();
+        var picker = new FileChooser();
+        picker.getExtensionFilters().add(
+            new FileChooser.ExtensionFilter("Images", "*.png", "*.jpg", "*.jpeg")
+        );
+        var file = picker.showOpenDialog(((Node)event.getSource()).getScene().getWindow());
+        imagePath.setText(file.getAbsolutePath());
+        showImage();
+    }
+
+    private void showImage() {
+        puzzlePreview.setImage(
+            new Image("file:///" + imagePath.getText())
+        );
     }
 }
