dee8c84c612310d8753008a46b533ae899ae3e03
asier-ochoa <78745593+asier-ochoa@users.noreply.github.com>
2024-04-12 15:22:16 -0500

    Sizing issues fixed and puzzle splitting added

--- src/main/java/StartMenu.java
+++ src/main/java/StartMenu.java
@@ -1,5 +1,3 @@
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
@@ -11,6 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -34,10 +33,12 @@ public class StartMenu {
     @FXML
     private void initialize(){
         configureSpinners();
-        configureImageView();
+        configureCanvas();
     }
 
-    private void configureImageView(){
+    private void configureCanvas(){
+        imageContainer.widthProperty().addListener(observable -> updateCanvasSize());
+        canvas.heightProperty().bind(imageContainer.heightProperty());
     }
 
     private void configureSpinners(){
@@ -47,6 +48,16 @@ public class StartMenu {
         factory2.setValue(8);
         verticalTilesInput.setValueFactory(factory1);
         horizontalTilesInput.setValueFactory(factory2);
+
+        factory1.valueProperty().addListener(observable -> computePuzzleLines());
+        factory2.valueProperty().addListener(observable -> computePuzzleLines());
+    }
+
+    // Must compute ratio based on fitHeight to size canvas to image
+    private void updateCanvasSize() {
+        if (mainImage != null){
+            canvas.setWidth(mainImage.getWidth() / mainImage.getHeight() * puzzlePreview.getFitHeight());
+        }
     }
 
     @FXML
@@ -59,6 +70,7 @@ public class StartMenu {
         var file = picker.showOpenDialog(((Node)event.getSource()).getScene().getWindow());
         imagePath.setText(file.getAbsolutePath());
         showImage();
+        computePuzzleLines();
     }
 
     @FXML
@@ -66,20 +78,33 @@ public class StartMenu {
         event.consume();
         showImage();
         computePuzzleLines();
-        System.out.println();
-
-        var s = (Stage)root.getScene().getWindow();
-        s.sizeToScene();
     }
 
     private void showImage() {
         mainImage = new Image("file:///" + imagePath.getText());
         puzzlePreview.setImage(mainImage);
-        imageContainer
+
+        // For some ungodly reason, this must be placed right after the image so that it doesn't get cut off
+        var s = (Stage)root.getScene().getWindow();
+        s.sizeToScene();
     }
 
     private void computePuzzleLines() {
-        canvas.setHeight(mainImage.getHeight());
-        canvas.setWidth(mainImage.getWidth());
+        updateCanvasSize();
+
+        var hLineAdvance = canvas.getWidth() / horizontalTilesInput.getValue();
+        var vLineAdvance = canvas.getHeight() / verticalTilesInput.getValue();
+        var gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        gc.setStroke(Color.BLUE);
+        // Draw horizontal lines
+        for (int i = 1; i <= verticalTilesInput.getValue(); ++i) {
+            gc.strokeLine(0, i * vLineAdvance, canvas.getWidth(), i * vLineAdvance);
+        }
+        // Draw vertical lines
+        for (int i = 0; i < horizontalTilesInput.getValue(); ++i) {
+            gc.strokeLine(i * hLineAdvance, 0, i * hLineAdvance, canvas.getHeight());
+        }
+
     }
 }
